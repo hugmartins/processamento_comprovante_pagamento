c37e1597ea72148e9a0d33e424ff90dc6685f0e9
hugmartins <hugomartins.hc@gmail.com>
2022-08-18 19:13:17 -0300

    feat - alterando logica de validacao de funcionarios com e sem comprovante

--- app/service/processamento_service.py
+++ app/service/processamento_service.py
@@ -5,9 +5,11 @@ from app.dto.models import Funcionario, ArquivoRetorno, DetalheArquivo
 from app.service.arquivo_service import validar_diretorio_liquido_folha, validar_diretorio_retorno_bancario, \
     carregar_lista_funcionarios_liquido_folha, carregar_retornos_bancario
 
-LISTA_COMPROVANTE_FUNCIONARIO_POR_FILIAL = {}
-LISTA_FUNCIONARIOS_COM_COMPROVANTE = []
-LISTA_FUNCIONARIOS_SEM_COMPROVANTE = []
+MAP_COMPROVANTE_FUNCIONARIO_POR_FILIAL = {}
+MAP_FUNCIONARIOS_SEM_COMPROVANTE_POR_FILIAL = {}
+
+LISTA_CPF_COM_COMPROVANTE = []
+LISTA_CPF_SEM_COMPROVANTE = []
 
 
 def iniciar_processamento():
@@ -25,21 +27,24 @@ def iniciar_processamento():
 
     localizar_dados_comprovante_funcionario(funcionarios_liquido_folha, lista_arquivos_retorno_bancario)
 
-    logging.info(f'funcionarios COM comprovante: {len(LISTA_FUNCIONARIOS_COM_COMPROVANTE)}')
-    logging.warning(f'funcionarios SEM comprovante: {len(LISTA_FUNCIONARIOS_SEM_COMPROVANTE)}')
+    logging.info(f'funcionarios COM comprovante: {len(LISTA_CPF_COM_COMPROVANTE)}')
+    logging.warning(f'funcionarios SEM comprovante: {len(LISTA_CPF_SEM_COMPROVANTE)}')
 
 
 def localizar_dados_comprovante_funcionario(funcionarios_liquido_folha: List[Funcionario],
                                             lista_arquivos_retorno_bancario: List[ArquivoRetorno]):
     for funcionario in funcionarios_liquido_folha:
+        comprovante_encontrado = False
+
         for arquivo_retorno in lista_arquivos_retorno_bancario:
             for detalhe in arquivo_retorno.lote.detalhe:
                 if funcionario.cpf in detalhe.segmento_b.numero_inscricao_favorecido:
+                    comprovante_encontrado = True
                     funcionario.dados_comprovante = detalhe
                     adicionar_funcionario_lista_funcionario_comprovante_por_filial(funcionario)
-                    LISTA_FUNCIONARIOS_COM_COMPROVANTE.append(funcionario.cpf)
 
-        validar_funcionario_com_comprovante(funcionario)
+        if comprovante_encontrado is False:
+            adicionar_funcionario_lista_funcionario_sem_comprovante_por_filial(funcionario)
 
 
 def validar_arquivos_existentes():
@@ -50,14 +55,24 @@ def validar_arquivos_existentes():
 def adicionar_funcionario_lista_funcionario_comprovante_por_filial(funcionario: Funcionario):
     codigo_filial = funcionario.descricao_filial.split("-")[0]
 
-    if codigo_filial in LISTA_COMPROVANTE_FUNCIONARIO_POR_FILIAL:
-        lista_funcionarios_filial = LISTA_COMPROVANTE_FUNCIONARIO_POR_FILIAL[codigo_filial]
+    if codigo_filial in MAP_COMPROVANTE_FUNCIONARIO_POR_FILIAL:
+        lista_funcionarios_filial = MAP_COMPROVANTE_FUNCIONARIO_POR_FILIAL[codigo_filial]
         lista_funcionarios_filial.append(funcionario)
-        LISTA_COMPROVANTE_FUNCIONARIO_POR_FILIAL[codigo_filial] = lista_funcionarios_filial
+        MAP_COMPROVANTE_FUNCIONARIO_POR_FILIAL[codigo_filial] = lista_funcionarios_filial
     else:
-        LISTA_COMPROVANTE_FUNCIONARIO_POR_FILIAL[codigo_filial] = [funcionario]
+        MAP_COMPROVANTE_FUNCIONARIO_POR_FILIAL[codigo_filial] = [funcionario]
+
+    LISTA_CPF_COM_COMPROVANTE.append(funcionario.cpf)
 
 
-def validar_funcionario_com_comprovante(funcionario: Funcionario):
-    if funcionario.cpf not in LISTA_FUNCIONARIOS_COM_COMPROVANTE:
-        LISTA_FUNCIONARIOS_SEM_COMPROVANTE.append(funcionario)
+def adicionar_funcionario_lista_funcionario_sem_comprovante_por_filial(funcionario: Funcionario):
+    codigo_filial = funcionario.descricao_filial.split("-")[0]
+
+    if codigo_filial in MAP_FUNCIONARIOS_SEM_COMPROVANTE_POR_FILIAL:
+        lista_funcionarios_filial = MAP_FUNCIONARIOS_SEM_COMPROVANTE_POR_FILIAL[codigo_filial]
+        lista_funcionarios_filial.append(funcionario)
+        MAP_FUNCIONARIOS_SEM_COMPROVANTE_POR_FILIAL[codigo_filial] = lista_funcionarios_filial
+    else:
+        MAP_FUNCIONARIOS_SEM_COMPROVANTE_POR_FILIAL[codigo_filial] = [funcionario]
+
+    LISTA_CPF_SEM_COMPROVANTE.append(funcionario.cpf)
